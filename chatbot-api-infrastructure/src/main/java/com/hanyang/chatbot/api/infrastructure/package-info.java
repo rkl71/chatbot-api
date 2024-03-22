4132e45b1ee1e4b54f0c11afc72ea486193764a5
rkl71 <renkelin00@gmail.com>
2024-03-22 12:26:02 +0800

    feature: init project

---
+++ chatbot-api-infrastructure/src/main/java/com/hanyang/chatbot/api/infrastructure/package-info.java
@@ -0,0 +1,1 @@
+package com.hanyang.chatbot.api.infrastructure;
---
+++ chatbot-api-interfaces/src/main/java/com/hanyang/chatbot/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.hanyang.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
